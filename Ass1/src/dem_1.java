8b85eefb2d03b3a018f1cda18ff39103bf987153
ashishj <ashishj@B106-05.soit.local>
2016-07-20 09:24:33 +1200

    added another person

--- Ass1/src/dem_1.java
+++ Ass1/src/dem_1.java
@@ -4,6 +4,12 @@ public class dem_1 {
 	public static void main(String[] args) {
 		person_info Person1 = new person_info ("John","012345","american");
 		Person1.displayinfo();
+		
+		
+		person_info Person2 = new person_info ("James","02216584","canada");
+		Person2.displayinfo();
+		
 	}
+	
 
 }
